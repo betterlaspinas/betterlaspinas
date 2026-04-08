4c2a46491e3b9dced8b8a93e54e38c8865960b8c
Martin-Enghoy <adrianenghoy@gmail.com>
2026-04-08 13:12:44 +0800

    feat(schema-org): add useSchemaOrg in base app.vue

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,14 @@ const config = useConfig()
 defineOgImage('DefaultBranding.takumi', {
   title: config.siteBrandName.value,
 })
+useSchemaOrg([
+  defineWebPage({
+    name: config.siteBrandName.value,
+  }),
+  defineWebSite({
+    name: config.siteBrandName.value,
+  }),
+])
 </script>
 
 <template>
