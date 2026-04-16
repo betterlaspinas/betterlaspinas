ea67b7ebf95b439690435d7c9a1435b2170fd4d0
Martin Enghoy <adrianenghoy@gmail.com>
2026-04-17 07:22:22 +0800

    feat(og-image): create sub page takumi template for og image

---
+++ app/components/OgImage/SubPageBranding.takumi.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// These props are automatically passed by nuxt-og-image
+const props = defineProps<{
+  title: string
+  description?: string
+}>()
+
+const { site, siteBrandName } = useConfig()
+
+const logoPath = computed(() => site.value.logo?.whitePng || '/logos/svg/BetterGov_Icon-White.png')
+</script>
+
+<template>
+  <!-- Takumi supports Tailwind CSS natively via the `tw` prop/classes -->
+  <div class="w-full h-full flex flex-col justify-between bg-gradient-to-br from-primary-500 to-primary-800 border-t-16 border-emerald-600 p-16">
+    <!-- Top Section: Title & Description -->
+    <div class="flex flex-col gap-10 w-full mt-16 items-start">
+      <h1 class="text-7xl font-bold leading-tight tracking-tight text-white">
+        {{ props.title }}
+      </h1>
+      <h1 class="text-3xl leading-tight tracking-tight text-white">
+        {{ props.description }}
+      </h1>
+    </div>
+    <div class="w-full flex flex-row justify-center items-center gap-4">
+      <img :src="logoPath" alt="Logo" class="object-contain" width="80" height="80">
+      <p class="text-3xl font-bold leading-tight tracking-tight text-white">
+        {{ siteBrandName }}
+      </p>
+    </div>
+  </div>
+</template>
